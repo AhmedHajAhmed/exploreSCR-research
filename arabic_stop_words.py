29a6fd4b6d76397abcd7bd21f459d02fd7ee304e
Ahmed Haj Ahmed <ahajahmed@haverford.edu>
2024-03-31 17:25:18 -0400

    created arabic_stop_words.py and added custom stop words list

---
+++ arabic_stop_words.py
@@ -0,0 +1,16 @@
+from preprocessing_utils import preprocess_text
+
+
+# Define an empty list to store the stop words
+arabic_stop_words = []
+
+# Open the text file and read each line in the file
+with open('arabic_stopwords_list.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        arabic_stop_words.append(preprocess_text(line.strip()))
+
+custom_arabic_stop_words = ["يا", "شو", "عم", "مش", "شي", "انو", "مين", "هيك", "هيدا", "اللي", "انك", "منك",
+                            "وانت", "كنت", "رح", "عنا", "ليش", "فيك", "هلق", "نحنا", "مو", "انتو"]
+
+arabic_stop_words = arabic_stop_words + custom_arabic_stop_words
+
